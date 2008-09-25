06df1600c824b71b3d0a45d57dcc372c893189d8
Kristian.Jerpetjoen <Kristian.Jerpetjoen@08951fc0-8b22-11dd-a9db-710176c1da70>
2008-09-25 18:11:17 +0000

    added Doxyfile ++ more automake structure
    git-svn-id: http://speutils.googlecode.com/svn/trunk@3 08951fc0-8b22-11dd-a9db-710176c1da70

---
+++ update.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+logfile=logfile.log
+echo "" > $logfile;
+run () {
+
+#	echo $1 >>  $logfile
+#	$1 >> $logfile	
+	
+	$1
+
+	if [ $? -eq 0 ]  ; then 
+		echo -e "\033[1m$1 [\E[0;32mOK\033[0m\033[1m]\033[0m" 
+	else 
+		echo -e "\033[1m$1 [\E[0;31mFAILED\033[0m\033[1m]\033[0m" 
+		exit 1;
+	fi
+}
+
+autotools() {
+	run "aclocal";
+
+	run "libtoolize --copy --force";
+
+	run "automake --add-missing --foreign" ;
+
+	run "autoconf";
+}
+
+enter_folder() {
+	if [ -d $1 ] ; then
+		echo -e "\033[1m\E[0;34mActive folder $1 \E[0m[\E[0;32mOK\033[0m\033[1m]\033[0m"
+		cd $1
+	else 
+		echo -e "\033[1m\E[44mActive folder  $1 \E[0m[\E[0;31mFAILED\033[0m\033[1m]\033[0m"
+		exit 1;
+	fi
+}
+
+
+autotools
+
+echo -e "Logfile" | cat $logfile
+
+
+
+
+
